7f18bf40c8299589337c6ae8c9723380c7207ddf
Dave Chevell <dchevell@atlassian.com>
2018-09-03 16:22:03 +1000

    Created Scheduler class to manage submission of scheduled futures to the executor

--- flask_executor/scheduler.py
+++ flask_executor/scheduler.py
@@ -1,5 +1,9 @@
 from concurrent.futures import Future
 from concurrent.futures._base import PENDING
+import threading
+import time
+
+from sortedcontainers import SortedList
 
 
 class ScheduledFuture(Future):
@@ -28,3 +32,31 @@ class ScheduledFuture(Future):
         future = self.executor._executor.submit(self.fn, *self.args, **self.kwargs)
         future.add_done_callback(self.set_result_from_future)
         return future
+
+
+class Scheduler:
+
+    def __init__(self, executor):
+        self.executor = executor
+        self.scheduled_futures = SortedList(key=lambda sf: sf.due)
+        self.scheduler = threading.Thread(target=self.cycle, daemon=True)
+
+    def add(self, fn, delay, *args, **kwargs):
+        if not self.scheduler.is_alive():
+            self.scheduler.start()
+        due = time.time() + delay
+        sf = ScheduledFuture(self.executor, fn, due, *args, **kwargs)
+        self.scheduled_futures.add(sf)
+        return sf
+
+    def cycle(self):
+        while True:
+            if not self.scheduled_futures:
+                time.sleep(0.1)
+                continue
+            sf = self.scheduled_futures[0]
+            if sf.due > time.time():
+                continue
+            if sf.pending():
+                sf.submit()
+            self.scheduled_futures.remove(sf)
